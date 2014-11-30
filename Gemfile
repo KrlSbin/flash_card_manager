6cc4ff81583c723c248a82b7366c5d563190a7aa
Kirill <kirill@rubymaker.(none)>
2014-11-30 22:56:19 +0000

    CRUD added

--- Gemfile
+++ Gemfile
@@ -14,4 +14,4 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'execjs'
 gem 'therubyracer'
-
+gem 'simple_form'
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,8 @@
 class Card < ActiveRecord::Base
+  validates :original_text, :translated_text, presence: true
+  validate :original_and_translated_not_equal
+
+  def original_and_translated_not_equal
+    errors.add(:translated_text, "can't be the same as original") if translated_text == original_text
+  end
 end
