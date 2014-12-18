f1a8f982a280104c952a946019d831015cbe4c34
Kirill Skryabin <goldpatch@mail.ru>
2014-12-18 11:15:02 +0000

    Card model tests. Revision 7.

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe Card do
   it "not save instance with identical original and translated text fields" do
     card = Card.new(original_text: "Word", translated_text: "Word")
-    expect(card.save).to be false
+    expect(card.valid?).to be false
   end
 
   it "save instance with different original and translated text fields" do
@@ -12,30 +12,26 @@ describe Card do
   end
 
   it "check positive translation" do
-    card = Card.new(original_text: "Word", translated_text: "Слово")
-    card.save
+    card = Card.create(original_text: "Word", translated_text: "Слово")
 
     expect(card.check_translation("Слово")).to be true
   end
 
   it "check negative translation" do
-    card = Card.new(original_text: "Word", translated_text: "Слово")
-    card.save
+    card = Card.create(original_text: "Word", translated_text: "Слово")
 
     expect(card.check_translation("Словечки")).to be false
   end
 
   it "update review date if translation is correct" do
-    card = Card.new(original_text: "Word", translated_text: "Слово")
-    card.save
+    card = Card.create(original_text: "Word", translated_text: "Слово")
     original_review_date = card.review_date
     card.check_translation("Слово")
     expect(card.review_date).to eql(original_review_date + 3.day)
   end
 
   it "not update review date if translation is not correct" do
-    card = Card.new(original_text: "Word", translated_text: "Слово")
-    card.save
+    card = Card.create(original_text: "Word", translated_text: "Слово")
     original_review_date = card.review_date
     card.check_translation("Словечко")
     expect(card.review_date).to eql(original_review_date)
