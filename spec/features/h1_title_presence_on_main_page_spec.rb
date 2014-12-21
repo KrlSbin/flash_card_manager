a68c2851b15a91263ee34c8cb0a4e06a6b5196ed
Kirill Skryabin <goldpatch@mail.ru>
2014-12-21 20:45:48 +0000

    6.Request specs. Revision 9.

--- spec/features/h1_title_presence_on_main_page_spec.rb
+++
@@ -1,8 +0,0 @@
-require "capybara/rspec"
-
-describe "open index page", type: :feature do
-  it "h1 presence on main page" do
-    visit "/"
-    expect(page).to have_content "Флэшкарточкер"
-  end
-end
