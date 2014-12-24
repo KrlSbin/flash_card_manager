c644892ac4ec0a2cf8c65cc14fc85783d10771f6
Kirill Skryabin <goldpatch@mail.ru>
2014-12-24 14:32:46 +0000

    6.Request specs. Revision 12.

--- spec/features/index_page_content_spec.rb
+++
@@ -1,8 +0,0 @@
-require "spec_helper"
-
-describe "index page content", type: :feature do
-  it "main title presence" do
-    visit "/"
-    expect(page).to have_content "Флэшкарточкер"
-  end
-end
--- spec/features/main_page_content_spec.rb
+++ spec/features/main_page_content_spec.rb
@@ -1,17 +1,22 @@
-require "spec_helper"
+require "rails_helper"
 
-describe "main page content", type: :feature do
+describe "main page content" do
   it "Reviewed card is not shown on main page" do
     card = create(:card, original_text: "card")
     card.review_date = Time.now + 3.days
     card.save
-    visit "/"
+    visit root_path
     expect(page).not_to have_content card.original_text
   end
 
   it "Unreviewed card is shown on main page" do
     create(:card, original_text: "card")
-    visit "/"
+    visit root_path
     expect(page).to have_content "card"
+  end
+
+  it "main title presence" do
+    visit root_path
+    expect(page).to have_content "Флэшкарточкер"
   end
 end
