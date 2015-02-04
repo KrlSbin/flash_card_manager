deda5376c5be77411f7e306a0ef5d723ce377adf
Kirill Skryabin <goldpatch@mail.ru>
2015-02-04 14:40:28 +0000

    8.Sorcery. Revision 14.

---
+++ spec/features/user_authentication_spec.rb
@@ -0,0 +1,84 @@
+require "rails_helper"
+
+describe "Authentication procedure" do
+  it "successful login\logout to portal" do
+    user = create(:user)
+    visit login_path
+    login_user(user.email, "1234")
+    expect(page).to have_content "Login successful"
+    click_link "Logout"
+    expect(page).to have_content "Logged out!"
+   end
+
+  it "unsuccessful login to portal with wrong password" do
+    user = create(:user)
+    visit login_path
+    login_user(user.email, "123")
+    expect(page).to have_content "Login failed"
+  end
+
+  it "successful new user registration and autologin" do
+    visit root_path
+    click_link "Register"
+    fill_in "Email", with: "1234"
+    fill_in "Password", with: "1234"
+    fill_in "Password confirmation", with: "1234"
+    click_button "Create User"
+    expect(page).to have_content "User was created."
+  end
+
+  it "unsuccessful new user registration with short password" do
+    visit root_path
+    click_link "Register"
+    fill_in "Email", with: "1234"
+    fill_in "Password", with: "12"
+    fill_in "Password confirmation", with: "12"
+    click_button "Create User"
+    expect(page).to have_content "Password is too short"
+  end
+  
+  it "unsuccessful new user registration with already used email" do
+    visit root_path
+    click_link "Register"
+    fill_in "Email", with: "1234"
+    fill_in "Password", with: "1234"
+    fill_in "Password confirmation", with: "1234"
+    click_button "Create User"
+    click_link "Logout"
+    click_link "Register"
+    fill_in "Email", with: "1234"
+    fill_in "Password", with: "1234"
+    fill_in "Password confirmation", with: "1234"
+    click_button "Create User"
+    expect(page).to have_content "Email has already been taken"
+  end
+  
+  it "unsuccessful new user registration with empty password" do
+    visit root_path
+    click_link "Register"
+    fill_in "Email", with: "1234"
+    fill_in "Password confirmation", with: "1234"
+    click_button "Create User"
+    expect(page).to have_content "Password is too short"
+  end
+
+  it "unsuccessful new user registration with empty password confirmation" do
+    visit root_path
+    click_link "Register"
+    fill_in "Email", with: "1234"
+    fill_in "Password", with: "1234"
+    click_button "Create User"
+    expect(page).to have_content "Password confirmation doesn't match Password"
+  end
+  
+  it "Usuccessful new user registration with wrong password confirmation" do
+    visit root_path
+    click_link "Register"
+    fill_in "Email", with: "1234"
+    fill_in "Password", with: "1234"
+    fill_in "Password confirmation", with: "12345"
+    click_button "Create User"
+    expect(page).to have_content "Password confirmation doesn't match Password"
+  end
+end
+
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe User do
+  it "do not save instance with less than 3 symbols in password" do
+    user = User.new(email: "123", password: "12", password_confirmation: "12")
+    expect(user.valid?).to be false
+  end
+
+  it "do not save instance with alredy used email" do
+    user1 = User.create(email: "123", password: "123", password_confirmation: "123")
+    user2 = User.new(email: "123", password: "321", password_confirmation: "321")
+    expect(user2.valid?).to be false
+  end
+
+  it "do not save instance without password" do
+    user = User.new(email: "123", password_confirmation: "321")
+    expect(user.valid?).to be false
+  end
+
+  it "do not save instance without confirmation" do
+    user = User.new(email: "123", password: "321")
+    expect(user.valid?).to be false
+  end
+
+  it "do not save instance with wrong confirmation" do
+    user = User.new(email: "123", password: "321", password_confirmation: "322")
+    expect(user.valid?).to be false
+  end
+end
