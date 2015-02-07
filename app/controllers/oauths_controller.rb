8efff9237e220919cf4a5487abafbd9f04f54f23
Kirill Skryabin <goldpatch@mail.ru>
2015-02-07 12:42:52 +0000

    8.Sorcery. Revision 18.

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -11,18 +11,19 @@ class OauthsController < ApplicationController
       redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
     else
       begin
-	@user = create_from(provider)
+        @user = create_from(provider)
 
-	reset_session
-	auto_login(@user)
-	redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
+        reset_session
+        auto_login(@user)
+        redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
       rescue
-	redirect_to root_path, alert: "Failed to login from #{provider.titleize}!"
+        redirect_to root_path, alert: "Failed to login from #{provider.titleize}!"
       end
     end
   end
 
   private
+
   def auth_params
     params.permit(:code, :provider)
   end
