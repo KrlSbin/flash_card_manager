1df6c4d17c1603d4afeb183d84bf1932f6697e5e
Kirill Skryabin <goldpatch@mail.ru>
2015-02-11 15:02:42 +0000

    8.Sorcery. Revision 28.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-
   skip_before_action :require_login, only: [:index, :new, :create]
 
   def index
@@ -33,18 +31,18 @@ class UsersController < ApplicationController
 
   def update
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: "Профиль обновлен." }
-        format.json { render :show, status: :ok, location: @user }
+      if current_user.update(user_params)
+        format.html { redirect_to current_user, notice: "Профиль обновлен." }
+        format.json { render :show, status: :ok, location: current_user }
       else
         format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: current_user.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def destroy
-    @user.destroy
+    current_user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: "Пользователь удален." }
       format.json { head :no_content }
@@ -52,10 +50,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-  def set_user
-    @user = current_user
-  end
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :authentications_attributes)
