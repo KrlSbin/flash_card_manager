c35d06865fdeca1572c98ad71c3bbeef9b7ccedd
Kirill Skryabin <goldpatch@mail.ru>
2015-03-23 15:15:22 +0000

    11.Card decks. Revision 20.

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -7,29 +7,34 @@ describe Card do
   end
 
   it "save instance with different original and translated text fields" do
-    card = Card.new(original_text: "Word", translated_text: "Слово", deck_id: 1)
+    card = Card.new(original_text: "Word", translated_text: "Слово",
+                                           deck_id: 1)
     expect(card.valid?).to be true
   end
 
   it "check positive translation" do
-    card = Card.create(original_text: "Word", translated_text: "Слово", deck_id: 1)
+    card = Card.create(original_text: "Word", translated_text: "Слово",
+                                              deck_id: 1)
     expect(card.check_translation("Слово")).to be true
   end
 
   it "check negative translation" do
-    card = Card.create(original_text: "Word", translated_text: "Слово", deck_id: 1)
+    card = Card.create(original_text: "Word", translated_text: "Слово",
+                                              deck_id: 1)
     expect(card.check_translation("Словечки")).to be false
   end
 
   it "update review date if translation is correct" do
-    card = Card.create(original_text: "Word", translated_text: "Слово", deck_id: 1)
+    card = Card.create(original_text: "Word", translated_text: "Слово",
+                                              deck_id: 1)
     original_review_date = card.review_date
     card.check_translation("Слово")
     expect(card.review_date).to eql(original_review_date + 3.day)
   end
 
   it "not update review date if translation is not correct" do
-    card = Card.create(original_text: "Word", translated_text: "Слово", deck_id: 1)
+    card = Card.create(original_text: "Word", translated_text: "Слово",
+                                              deck_id: 1)
     original_review_date = card.review_date
     card.check_translation("Словечко")
     expect(card.review_date).to eql(original_review_date)
