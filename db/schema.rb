918c280d3de4dc4dbb9707d1ac2d75e1ff66db87
Kirill Skryabin <goldpatch@mail.ru>
2015-04-16 23:34:53 +0300

    Change card date to datetime

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150415045007) do
+ActiveRecord::Schema.define(version: 20150416102100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20150415045007) do
   create_table "cards", force: true do |t|
     t.text     "original_text"
     t.text     "translated_text"
-    t.date     "review_date"
+    t.datetime "review_date"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
