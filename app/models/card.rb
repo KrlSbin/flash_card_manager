7a89ffa4685837b55b66289874856fec31f5a7f5
Kirill Skryabin <goldpatch@mail.ru>
2015-04-19 01:18:12 +0300

    refactor case-when blocks and set_default_attributes method

--- app/models/card.rb
+++ app/models/card.rb
@@ -11,11 +11,20 @@ class Card < ActiveRecord::Base
   validates :original_text, :translated_text, :deck_id, presence: true
   validate :original_and_translated_not_equal
 
-  before_create :set_review_date, :set_box_number, :set_attempt
+  before_create :set_default_attributes
 
   def check_translation(translation)
     if translated_text == translation
-      case box_number
+      translation_is_correct
+    else
+      translation_is_incorrect
+    end
+  end
+
+  private
+
+  def translation_is_correct
+    case box_number
       when 1
         update_attributes(review_date: Time.now + 12.hours, box_number: 2)
       when 2
@@ -28,11 +37,13 @@ class Card < ActiveRecord::Base
         update_attributes(review_date: Time.now + 1.month, box_number: 6)
       when 6
         update_attributes(review_date: Time.now + 1.month)
-      end
-      update_attributes(attempt: 0)
-      return true
-    else
-      case attempt
+    end
+    update_attributes(attempt: 0)
+    return true
+  end
+
+  def translation_is_incorrect
+    case attempt
       when 0
         update_attributes(attempt: 1)
       when 1
@@ -41,22 +52,13 @@ class Card < ActiveRecord::Base
         update_attributes(review_date: Time.now + 12.hours,
                           box_number: 1,
                           attempt: 0)
-      end
-      return false
     end
+    return false
   end
 
-  private
-
-  def set_review_date
+  def set_default_attributes
     self.review_date = Time.now
-  end
-
-  def set_box_number
     self.box_number = 1
-  end
-
-  def set_attempt
     self.attempt = 0
   end
 
