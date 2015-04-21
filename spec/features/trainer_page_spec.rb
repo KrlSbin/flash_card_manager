6eb8ff4d171509aa64c194fe9045371759d27773
Kirill Skryabin <goldpatch@mail.ru>
2015-04-22 01:43:26 +0300

    Update tests according to new additional Levenshtein distance logic

--- spec/features/trainer_page_spec.rb
+++ spec/features/trainer_page_spec.rb
@@ -39,7 +39,7 @@ describe "Trainer page" do
     expect(page).to have_content "Sea"
   end
 
-  it "show unreviewed card from current deck and get incorrect translation" do
+  it "show unreviewed card from current deck and put typo in translation" do
     user = create(:user)
     login_user(user.email, "1234")
     click_link "Добавить колоду"
@@ -55,6 +55,26 @@ describe "Trainer page" do
     click_link "Сделать текущей"
     visit root_path
     fill_in "Перевод:", with: "Мор"
+    click_button "Проверить"
+    expect(page).to have_content "Очепятка!"
+  end
+
+  it "show unreviewed card from current deck and put wrong translation" do
+    user = create(:user)
+    login_user(user.email, "1234")
+    click_link "Добавить колоду"
+    fill_in "deck_name", with: "current"
+    click_button "Create Deck"
+    click_link "Назад"
+    click_link "Добавить карту"
+    fill_in "card_original_text", with: "Sea"
+    fill_in "card_translated_text", with: "Море"
+    click_button "Create Card"
+    click_link "Назад"
+    click_link "Все колоды пользователя"
+    click_link "Сделать текущей"
+    visit root_path
+    fill_in "Перевод:", with: "Моречко"
     click_button "Проверить"
     expect(page).to have_content "Неправильно!"
   end
@@ -79,7 +99,7 @@ describe "Trainer page" do
     expect(page).to have_content "Правильно!"
   end
 
-  it "show unreviewed card from non-current deck and get correct translation" do
+  it "show unreviewed card from non-current deck and put right translation" do
     user = create(:user)
     login_user(user.email, "1234")
     click_link "Добавить колоду"
@@ -91,13 +111,26 @@ describe "Trainer page" do
     fill_in "card_translated_text", with: "Море"
     click_button "Create Card"
     visit root_path
-    expect(page).to have_content "Sea"
-    fill_in "Перевод:", with: "Мор"
-    click_button "Проверить"
-    expect(page).to have_content "Неправильно!"
     fill_in "Перевод:", with: "Море"
     click_button "Проверить"
     expect(page).to have_content "Правильно!"
+  end
+
+  it "show unreviewed card from non-current deck and typo in translation" do
+    user = create(:user)
+    login_user(user.email, "1234")
+    click_link "Добавить колоду"
+    fill_in "deck_name", with: "current"
+    click_button "Create Deck"
+    click_link "Назад"
+    click_link "Добавить карту"
+    fill_in "card_original_text", with: "Sea"
+    fill_in "card_translated_text", with: "Море"
+    click_button "Create Card"
+    visit root_path
+    fill_in "Перевод:", with: "Моер"
+    click_button "Проверить"
+    expect(page).to have_content "Очепятка!"
   end
 
   it "main title presence" do
