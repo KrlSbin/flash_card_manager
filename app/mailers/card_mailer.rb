2a673c68495c62e83bb656335b35f0c20ecee0cd
Kirill Skryabin <goldpatch@mail.ru>
2015-05-01 14:06:43 +0300

    Change trainer link to root_url

--- app/mailers/card_mailer.rb
+++ app/mailers/card_mailer.rb
@@ -2,7 +2,6 @@ class CardMailer < ActionMailer::Base
   default from: ENV["MAILER_ADDR"]
   def cards_to_review(user)
     @user = user
-    @url = ENV["TRAINER_URL"]
     mail(to: @user.email, subject: "You have new cards for review!")
   end
 end
